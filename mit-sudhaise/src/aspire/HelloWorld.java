825820c3206f7c8c0d99b22eec23efbd7be9098a
sudhaise <sudhaise1741@gmail.com>
2022-02-23 11:47:39 +0530

    my hello world

---
+++ mit-sudhaise/src/aspire/HelloWorld.java
@@ -0,0 +1,11 @@
+package aspire;
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java.....");
+	}
+}
+/*
+Purpose
+1. In this program you understood how to create a project, how to create a class and how to execute and print
+"Welcome To Java" on the console.
+*/
